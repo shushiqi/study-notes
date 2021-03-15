2521df0da3557390e97ce8ad908df358fdbba01b
liuhang <1539588925@qq.com>
2021-03-15 21:43:40 +0800

    collapse

---
+++ vue-components/collapse/my-collapse-item.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container" @click="isContentShow">
+    <div class="header">
+      <h4>{{ title }}</h4>
+    </div>
+    <div class="content" v-show="contentShow">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  inject: ["collapse"],
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+  data() {
+    return {
+      //   contentShow: false,
+    };
+  },
+  computed: {
+    contentShow: function () {
+      return this.collapse.value.indexOf(this.name) > -1;
+    },
+  },
+  methods: {
+    isContentShow() {
+      this.collapse.$emit("my-collapse-item-click", this);
+    },
+  },
+  created() {},
+};
+</script>
+
+<style>
+</style>
---
+++ vue-components/collapse/my-collapse.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Array,
+      default: [],
+    },
+  },
+  provide() {
+    return {
+      collapse: this,
+    };
+  },
+  created() {
+    this.$on("my-collapse-item-click", (item) => {
+      let index = this.value.indexOf(item.name);
+      if (index > -1) {
+        this.value.splice(index, 1);
+      } else {
+        this.value.push(item.name);
+      }
+    });
+  },
+};
+</script>
+
+<style>
+</style>
